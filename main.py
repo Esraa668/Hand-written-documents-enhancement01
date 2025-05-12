5c23a5d3299ccdfa8f8276bff9a960409f9bbcc5
ShroukFathy <fathy.shrouk@gmail.com>
2025-05-12 15:08:07 +0300

    add grayscale conversion and file check

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ import cv2
 import numpy as np
 import os
 from matplotlib import pyplot as pl
+
+def enhance_document(image_path, output_dir="output"):
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    original = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE) #grayscale conversion
+    if original is None:
+        raise ValueError(f"Image not found at {image_path}") #checks if image exists in the destined path
